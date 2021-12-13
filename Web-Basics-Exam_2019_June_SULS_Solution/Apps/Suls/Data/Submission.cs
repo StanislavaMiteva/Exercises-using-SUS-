b443aac0ce576b114905a533e6c6dffb9f9c7edc
StanislavaMiteva <stanislava.miteva@gmail.com>
2021-12-13 22:25:00 +0200

    Create models and database.

---
+++ Web-Basics-Exam_2019_June_SULS_Solution/Apps/Suls/Data/Submission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Suls.Data
+{
+    public class Submission
+    {
+        public Submission()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(800)]
+        public string Code { get; set; }
+
+        public int AchievedResult  { get; set; }
+
+        public DateTime CreatedOn  { get; set; }
+                
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        [ForeignKey(nameof(Problem))]
+        public string ProblemId { get; set; }
+
+        public virtual Problem Problem { get; set; }
+    }
+}
---
+++ Web-Basics-Exam_2019_June_SULS_Solution/Apps/Suls/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using Suls.Data;
+using Microsoft.EntityFrameworkCore;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+
+namespace Suls
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+            new ApplicationDbContext().Database.Migrate();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            
+        }
+    }
+}
